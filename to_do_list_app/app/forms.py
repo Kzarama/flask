db4d013039b2f4c7d888ef2f0fbecd3dea5d20e2
kz <zaramaluna1999@hotmail.com>
2020-11-14 20:36:29 -0500

    methods for create todos added

--- to_do_list_app/app/forms.py
+++ to_do_list_app/app/forms.py
@@ -2,7 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Send')
+
+
+class TodoForm(FlaskForm):
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Create')
